223d7507a50e35ff456c49d87c78b01c117682c0
Linke <linke@duck.com>
2023-02-14 04:09:27 +0300

    Completed task 7

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var fighterWeight1 = 78.2;
         var fighterWeight2 = 82.7;
-        var sumWeight = fighterWeight1 + fighterWeight2;
-        System.out.println("Общий вес бойцов равен " + sumWeight + " кг");
-        var difWeight = fighterWeight2 - fighterWeight1;
-        System.out.println("Разница в весе составляет " + difWeight + " кг");
+        var difWeight1 = fighterWeight2 - fighterWeight1;
+        System.out.println("Разница в весе составляет " + difWeight1 + " кг");
+        var difWeight2 = fighterWeight2 % fighterWeight1;
+        System.out.println("Разница в весе составляет " + difWeight2 + " кг");
     }
 }
