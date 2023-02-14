27b357c2226207659f945a2a830f6b3213327473
Linke <linke@duck.com>
2023-02-14 03:40:02 +0300

    Completed task 3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
         System.out.println(dog);
+        var cat = 3.6;
         System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
         System.out.println(paper);
     }
 }
