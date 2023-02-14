7d9806e1e8e5e2c36c53019de6ced93782e4937b
Linke <linke@duck.com>
2023-02-14 22:20:56 +0300

    Completed task 8.1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var fighterWeight1 = 78.2;
-        var fighterWeight2 = 82.7;
-        var difWeight1 = fighterWeight2 - fighterWeight1;
-        System.out.println("Разница в весе составляет " + difWeight1 + " кг");
-        var difWeight2 = fighterWeight2 % fighterWeight1;
-        System.out.println("Разница в весе составляет " + difWeight2 + " кг");
+        var allWorkTime = 640;
+        var workTime = 8;
+        var workers = allWorkTime / workTime;
+        System.out.println("Всего работников в компании " + workers + " человек");
     }
 }
