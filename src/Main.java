15cef0a95f69e228aff42f2912e32810ac32b466
Linke <linke@duck.com>
2023-02-14 04:04:50 +0300

    Completed task 6

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+        var fighterWeight1 = 78.2;
+        var fighterWeight2 = 82.7;
+        var sumWeight = fighterWeight1 + fighterWeight2;
+        System.out.println("Общий вес бойцов равен " + sumWeight + " кг");
+        var difWeight = fighterWeight2 - fighterWeight1;
+        System.out.println("Разница в весе составляет " + difWeight + " кг");
     }
 }
