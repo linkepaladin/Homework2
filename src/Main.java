aface49348eb24a842d50ff94870baab7be786bf
Linke <linke@duck.com>
2023-02-14 22:27:12 +0300

    Completed task 8.2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         var workTime = 8;
         var workers = allWorkTime / workTime;
         System.out.println("Всего работников в компании " + workers + " человек");
+        workers = workers + 94;
+        workTime = allWorkTime / workers;
+        System.out.println("Если в компании работает " + workers + " человек, то всего " + workTime +
+                            " часа работы может быть поделено между сотрудниками");
     }
 }
