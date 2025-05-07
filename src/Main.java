71977a8800194ec40584e2318f1de0e4b65d0e0f
linke <linke@duck.com>
2025-05-07 15:55:21 +0300

    Complete Task4

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
         task1();
         task2();
         task3();
-        }
+        task4();
+    }
     public static void task1() {
         byte a = 127;
         short b = -32768;
@@ -35,6 +36,21 @@ public class Main {
         int totalStudents = ludmilaPavlovnaStudentCount + annaSergeevnaStudentCount + ekaterinaAndreevnaStudentCount;
         int paperPerStudent = totalPaperSheets / totalStudents;
         System.out.println("На каждого ученика рассчитано " + paperPerStudent + " листов бумаги");
+    }
+    public static void task4() {
+        byte bottlesPer2Minutes = 16; // Производительность за 2 минуты
+        byte timeIncrement = 2; // Шаг времени (в минутах)
+        int bottlesPer1Minutes = bottlesPer2Minutes / timeIncrement; // Производительность за 1 минуту
+        int bottlesPer20Minutes = 20 * bottlesPer1Minutes; // Производительность за 20 минут
+        int minutesInDay = 60 * 24; // Количество минут в сутках
+        int bottlesPerDay = minutesInDay * bottlesPer1Minutes; // Производительность за 1 сутки
+        int bottlesPer3Days = 3 * bottlesPerDay; // Производительность за 3 дня
+        int daysInMonth = 30; // Количество дней в месяце
+        int bottlesPerMonth = daysInMonth * bottlesPerDay; // Производителность за месяц
+        System.out.println("За 20 минут машина произвела " + bottlesPer20Minutes + " штук бутылок");
+        System.out.println("За сутки машина произвела " + bottlesPerDay + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + bottlesPer3Days + " штук бутылок");
+        System.out.println("За 1 месяц машина произвела " + bottlesPerMonth + " штук бутылок");
 
     }
 }
